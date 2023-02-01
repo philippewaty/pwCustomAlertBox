bdc4ce59bc0bb505be39e7041e4f957c4a39c5e3
Philippe Waty <47305204+philippewaty@users.noreply.github.com>
2023-02-01 17:11:15 +0100

    Package nuget syntax correction

--- pwCustomAlertBox/pack nuget.cmd
+++ pwCustomAlertBox/pack nuget.cmd
@@ -2,6 +2,6 @@ if not exist NuGet mkdir NuGet
 
 del /Q NuGet\*.*
 
-NuGet.exe pack pwExtensionsLibrary.nuspec -OutputDirectory NuGet -IncludeReferencedProjects -Properties Configuration=Release;Platform=AnyCPU -Build
+NuGet.exe pack pwCurstomAlertBox.nuspec -OutputDirectory NuGet -IncludeReferencedProjects -Properties Configuration=Release;Platform=AnyCPU -Build
 
 rem pause
